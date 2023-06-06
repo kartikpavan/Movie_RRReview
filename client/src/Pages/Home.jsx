48d018dcd2ad2ba3155c13a6ac934f4db02b9a50
kartikpavan <kartik.varanasi@yahoo.com>
2023-06-06 15:04:20 +0530

    (client) : User AUTH UI complete

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,32 +1,10 @@
 import React from "react";
-import { useAuthContext } from "../context/authContext";
-import { useNavigate } from "react-router-dom";
+import NotVerifiedBanner from "../components/NotVerifiedBanner";
 
 const Home = () => {
-   const navigate = useNavigate();
-
-   const { authInfo } = useAuthContext();
-   console.log("home : ", authInfo);
-   const isVerified = authInfo.profile?.isVerified;
-
-   const navigateToVerificationPage = () => {
-      navigate("/auth/email-verification", { state: { user: authInfo.profile } });
-   };
-
    return (
       <>
-         {/* if user is verified then show the NOT VERIFIED POP UP */}
-         {authInfo.isLoggedIn && !isVerified ? (
-            <div className="bg-info text-info-content w-full text-center p-1">
-               Seems like you haven't verified your account,{" "}
-               <button
-                  onClick={navigateToVerificationPage}
-                  className="link link-base font-semibold"
-               >
-                  click here to Verifiy your Account
-               </button>
-            </div>
-         ) : null}
+         <NotVerifiedBanner />
       </>
    );
 };
---
+++ client/src/components/NotVerifiedBanner.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../context/authContext";
+
+const NotVerifiedBanner = () => {
+   const navigate = useNavigate();
+   const { authInfo } = useAuthContext();
+   const isVerified = authInfo.profile?.isVerified;
+
+   const navigateToVerificationPage = () => {
+      navigate("/auth/email-verification", { state: { user: authInfo.profile } });
+   };
+
+   return (
+      <>
+         {/* if user is verified then show the NOT VERIFIED POP UP */}
+         {authInfo.isLoggedIn && !isVerified ? (
+            <div className="bg-info text-info-content w-full text-center p-1">
+               Seems like you haven't verified your account,{" "}
+               <button
+                  onClick={navigateToVerificationPage}
+                  className="link link-base font-semibold"
+               >
+                  click here to Verifiy your Account
+               </button>
+            </div>
+         ) : null}
+      </>
+   );
+};
+
+export default NotVerifiedBanner;
